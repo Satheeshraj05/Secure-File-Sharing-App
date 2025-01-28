0cb7d0f8e0611b2f2333b74965dc134ee95c1b9b
Vicky <mac@Vicky.local>
2025-01-29 01:23:27 +0530

    Update requirement.txt

--- secure-file-sharing/backend/build.sh
+++ secure-file-sharing/backend/build.sh
@@ -2,6 +2,7 @@
 # exit on error
 set -o errexit
 
+cd secure-file-sharing/backend  # Navigate to the backend directory
 python -m pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
